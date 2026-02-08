b12f2678c5dd51c03a8fe4652f30be8f1b9c479f
Rafael Velasco Wagner <rafmenthe52@gmail.com>
2026-02-08 11:43:40 +0100

    Update player

--- player.c
+++ player.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_OBJECTS 1
+
 /**
  * @brief Space
  *
@@ -11,11 +13,9 @@
 struct _Player {
   Id id;                    /*!< Id number of the space, it must be unique */
   char name[WORD_SIZE + 1]; /*!< Name of the space */
-  Id north;                 /*!< Id of the space at the north */
-  Id south;                 /*!< Id of the space at the south */
-  Id east;                  /*!< Id of the space at the east */
-  Id west;                  /*!< Id of the space at the west */
+  id location;
   Bool object;              /*!< Whether the space has an object or not */
+  id objects[MAX_OBJECTS];
 };
 
 /** space_create allocates memory for a new space
@@ -35,12 +35,12 @@ Player* player_create(Id id) {
   /* Initialization of an empty space*/
   newPlayer->id = id;
   newPlayer->name[0] = '\0';
-  newPlayer->north = NO_ID;
-  newPlayer->south = NO_ID;
-  newPlayer->east = NO_ID;
-  newPlayer->west = NO_ID;
+  newPlayer->location;
   newPlayer->object = FALSE;
-
+  for(i=0, i<MAX_OBJECTS, i++){
+    newPlayer->objects[i] =NO_ID;
+  }
+  
   return newPlayer;
 }
 
@@ -78,11 +78,11 @@ const char* player_get_name(Player* player) {
   return player->name;
 }
 
-Status player_set_north(Player* player, Id id) {
+Status player_set_location(Player* player, Id id) {
   if (!player || id == NO_ID) {
     return ERROR;
   }
-  player->north = id;
+  player->location = id;
   return OK;
 }
 
@@ -90,52 +90,7 @@ Id player_get_north(Player* player) {
   if (!player) {
     return NO_ID;
   }
-  return player->north;
-}
-
-Status player_set_south(Player* player, Id id) {
-  if (!player || id == NO_ID) {
-    return ERROR;
-  }
-  player->south = id;
-  return OK;
-}
-
-Id player_get_south(Player* player) {
-  if (!player) {
-    return NO_ID;
-  }
-  return player->south;
-}
-
-Status player_set_east(Player* player, Id id) {
-  if (!player || id == NO_ID) {
-    return ERROR;
-  }
-  player->east = id;
-  return OK;
-}
-
-Id player_get_east(Player* player) {
-  if (!player) {
-    return NO_ID;
-  }
-  return player->east;
-}
-
-Status player_set_west(Player* player, Id id) {
-  if (!player || id == NO_ID) {
-    return ERROR;
-  }
-  player->west = id;
-  return OK;
-}
-
-Id player_get_west(Player* player) {
-  if (!player) {
-    return NO_ID;
-  }
-  return player->west;
+  return player->location;
 }
 
 Status player_set_object(Player* player, Bool value) {
@@ -165,29 +120,11 @@ Status player_print(Player* player) {
   fprintf(stdout, "--> Player (Id: %ld; Name: %s)\n", player->id, player->name);
 
   /* 2. For each direction, print its link */
-  idaux = player_get_north(player);
-  if (idaux != NO_ID) {
-    fprintf(stdout, "---> North link: %ld.\n", idaux);
-  } else {
-    fprintf(stdout, "---> No north link.\n");
-  }
-  idaux = player_get_south(player);
-  if (idaux != NO_ID) {
-    fprintf(stdout, "---> South link: %ld.\n", idaux);
-  } else {
-    fprintf(stdout, "---> No south link.\n");
-  }
-  idaux = player_get_east(player);
-  if (idaux != NO_ID) {
-    fprintf(stdout, "---> East link: %ld.\n", idaux);
-  } else {
-    fprintf(stdout, "---> No east link.\n");
-  }
-  idaux = player_get_west(player);
-  if (idaux != NO_ID) {
-    fprintf(stdout, "---> West link: %ld.\n", idaux);
-  } else {
-    fprintf(stdout, "---> No west link.\n");
+  idaux = player_get_location(player);
+  if(idaux 1= NO_ID){
+    fprintf(stdout, "Location id is: %ld",idaux )
+  }else{
+    fprintf(stdout, "No location id found", idaux)
   }
 
   /* 3. Print if there is an object in the space or not */
