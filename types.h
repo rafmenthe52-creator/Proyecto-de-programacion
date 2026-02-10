d464904a88bff331c15b9686ab1f1a06e7d5f6ce
Rafael Velasco Wagner <rafmenthe52@gmail.com>
2026-02-10 11:25:49 +0100

    types documentation

--- types.h
+++ types.h
@@ -14,12 +14,32 @@
 #define WORD_SIZE 1000
 #define NO_ID -1
 
+/**
+ * @brief Id
+ *
+ * Data type for identifiers. They are actually type long
+ */
 typedef long Id;
 
+/**
+ * @brief Bool
+ *
+ * Data type that shows wheter something is true or false. FALSE=0; TRUE=1
+ */
 typedef enum { FALSE, TRUE } Bool;
 
+/**
+ * @brief Status
+ *
+ * Data type for errorproofing. ERROR=0 ; OK=1
+ */
 typedef enum { ERROR, OK } Status;
 
+/**
+ * @brief Direction
+ *
+ * Data type that indicates the direction. N=0 ; S=1 ; E=2 ; W=3
+ */
 typedef enum { N, S, E, W } Direction;
 
 #endif
