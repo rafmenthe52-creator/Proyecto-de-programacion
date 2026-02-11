29a0453f404f095179bc053d927b68c20d1027c2
Rafael Velasco Wagner <rafmenthe52@gmail.com>
2026-02-11 21:23:12 +0100

    Game_reader briefs

--- game_reader.h
+++ game_reader.h
@@ -4,10 +4,35 @@
 #include "game.h"
 #include <string.h>
 
-
+/**
+ * @brief This function reads the space data from the file, adds the data to a struct of type space and then adds a pointer to that struct to the game(struct)
+ * @author Rafael Velasco-RV
+ *
+ * @param game a pointer to the struct in which the spaces will be saved
+ * @param filename a pointer to the name of the data file
+ * @return the Status of the function, either OK or ERROR
+ */
 Status game_reader_load_spaces(Game *game, char *filename);
 
-Status game_reader_add_space(Game *game, Space *space);
+/**
+ * @brief This function reads the object data from the file, adds that data to a struct of type object and then adds a pointer to that struct to the game(struct)
+ * @author Rafael Velasco-RV
+ *
+ * @param game a pointer to the struct in which the spaces will be saved
+ * @param filename a pointer to the name of the data file
+ * @return the Status of the function, either OK or ERROR
+ */
+Status game_reader_load_objects(Game* game, char* filename);
+
+/**
+ * @brief This function reads the player data from the file, adds that data to a struct of type player and then adds a pointer to that struct to the game(struct)
+ * @author Rafael Velasco-RV
+ *
+ * @param game a pointer to the struct in which the spaces will be saved
+ * @param filename a pointer to the name of the data file
+ * @return the Status of the function, either OK or ERROR
+ */
+Status game_reader_load_player(Game* game, char* filename);
 
 /*Anotación RV: En un futuro hay que llamar estas funciones desde otro .c, dado que ahora mismo no se están usando*/
 #endif
